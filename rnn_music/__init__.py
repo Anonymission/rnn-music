6232b75e2cac8b1bed5e293089920bd74b1778f2
Anonymission <marrs2k@gmail.com>
2017-08-04 22:38:43 -0400

    added docstrings; prebuilt bach cholares database

--- rnn_music/__init__.py
+++ rnn_music/__init__.py
@@ -1,267 +1,58 @@
-from mygrad import Tensor
-from mygrad.nnet.activations import sigmoid, tanh, softmax
-from mygrad.nnet.layers import dense
-from mygrad.math import log
+from mido import Message, MidiFile, MidiTrack
 import numpy as np
 import mido
 import os.path
 import pickle
 
-# last ky is 'split' key (indicates when to move to next line)
+# last key is 'split' key (indicates when to move to next line)
 keys = ["!", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", "0", "1", "2", "3", "4", "5", "6",
         "7", "8", "9", ":", ";", "<", "=", ">", "?", "@", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
         "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "[", "]", "^", "_", "`", "a", "b", "c", "d", "e", "f", "g", "h", "i",
         "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "{", "|", " "]
 
 rnn_data = []
-with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "music_gen.pickle"), 'rb') as f:
-    rnn_data = pickle.load(f)
 
 
-def train_model(data, k=5, hd=700, wub=None, hin1=None, hin2=None) :
+def import_dir(directory) :
     """
-    Trains model of RNN
+    Imports all valid midi files in directory
 
     Parameters
     -----------
-    data: ndarray
-        array of data (what form?)
-    k: int; default = 48 (.5 beats)
-        number of backprops to undergo
-    hd: int; default = 100
-        size of hidden dimension
-    wub: tuple of Tensors; optional
-        weights from previous training
-    hin: Tensor; optional
-        output from previous training
+    directory: r"PATH"
+        path to directory of midi files
     """
-    # initialize weights
-    md = len(keys)
-    wz1 = Tensor(he_normal((hd, hd))) if wub is None else wub[0, 0]
-    uz1 = Tensor(he_normal((hd, md))) if wub is None else wub[0, 1]
-    bz1 = Tensor(np.zeros((hd, 1), dtype=wz1.dtype)) if wub is None else wub[0, 2]
-    wr1 = Tensor(he_normal((hd, hd))) if wub is None else wub[0, 3]
-    ur1 = Tensor(he_normal((hd, md))) if wub is None else wub[0, 4]
-    br1 = Tensor(np.zeros((hd, 1), dtype=wr1.dtype)) if wub is None else wub[0, 5]
-    wh1 = Tensor(he_normal((hd, hd))) if wub is None else wub[0, 6]
-    uh1 = Tensor(he_normal((hd, md))) if wub is None else wub[0, 7]
-    bh1 = Tensor(np.zeros((hd, 1), dtype=wh1.dtype)) if wub is None else wub[0, 8]
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith(".mid"):
+            import_midi(os.path.join(directory, filename))
+        else:
+            continue
 
-    wz2 = Tensor(he_normal((hd, hd))) if wub is None else wub[1, 0]
-    uz2 = Tensor(he_normal((hd, hd))) if wub is None else wub[1, 1]
-    bz2 = Tensor(np.zeros((hd, 1), dtype=wz2.dtype)) if wub is None else wub[1, 2]
-    wr2 = Tensor(he_normal((hd, hd))) if wub is None else wub[1, 3]
-    ur2 = Tensor(he_normal((hd, hd))) if wub is None else wub[1, 4]
-    br2 = Tensor(np.zeros((hd, 1), dtype=wr2.dtype)) if wub is None else wub[1, 5]
-    wh2 = Tensor(he_normal((hd, hd))) if wub is None else wub[1, 6]
-    uh2 = Tensor(he_normal((hd, hd))) if wub is None else wub[1, 7]
-    bh2 = Tensor(np.zeros((hd, 1), dtype=wh2.dtype)) if wub is None else wub[1, 8]
-
-    v = Tensor(he_normal((md, hd))) if wub is None else wub[2]
-    c = Tensor(np.zeros((md, 1), dtype=wh2.dtype)) if wub is None else wub[3]
-
-    wub = ((wz1, uz1, bz1, wr1, ur1, br1, wh1, uh1, bh1), (wz2, uz2, bz2, wr2, ur2, br2, wh2, uh2, bh2), v, c)
-
-    hin1 = Tensor(he_normal((hd, 1))) if hin1 is None else hin1
-    hin2 = Tensor(he_normal((hd, 1))) if hin2 is None else hin2
-    loss = Tensor(0)
-
-    for i in range(len(data) - 1) :
-        x = one_hot_encode(data, i)
-
-        hout1, hout2 = forward_pass(x, hin1, hin2, wub[:2])
-
-        x_next = one_hot_encode(data, i + 1)
-
-        loss += cross_entropy(softmax(dense(v, hout2) + c), x_next)
-        loss.backward()
-        loss.null_creators()
-
-        if (i != 0) and (i % k == 0):
-            print("reset")
-            for params in wub[:2] :
-                for param in params :
-                    rate = 1.
-                    sgd(param, rate)
-            for param in wub[2:] :
-                rate = 1.
-                sgd(param, rate)
-
-            loss = Tensor(0)
-            wz1 = Tensor(wz1.data)
-            uz1 = Tensor(uz1.data)
-            bz1 = Tensor(bz1.data)
-            wr1 = Tensor(wr1.data)
-            ur1 = Tensor(ur1.data)
-            br1 = Tensor(br1.data)
-            wh1 = Tensor(wh1.data)
-            uh1 = Tensor(uh1.data)
-            bh1 = Tensor(bh1.data)
-            wz2 = Tensor(wz2.data)
-            uz2 = Tensor(uz2.data)
-            bz2 = Tensor(bz2.data)
-            wr2 = Tensor(wr2.data)
-            ur2 = Tensor(ur2.data)
-            br2 = Tensor(br2.data)
-            wh2 = Tensor(wh2.data)
-            uh2 = Tensor(uh2.data)
-            bh2 = Tensor(bh2.data)
-            v = Tensor(v.data)
-            c = Tensor(c.data)
-            hin1 = Tensor(hout1.data)
-            hin2 = Tensor(hout2.data)
-
-            wub = ((wz1, uz1, bz1, wr1, ur1, br1, wh1, uh1, bh1), (wz2, uz2, bz2, wr2, ur2, br2, wh2, uh2, bh2), v, c)
-            x = x_next
-        else :
-            hin1 = hout1
-            hin2 = hout2
-            x = x_next
-
-        if i % 1000 == 0 :
-            rnn_data.append([wub, hin1, hin2, i])
-            save()
-    rnn_data.append([None])
-    return wub, hout1, hout2
-
-
-def test_model(wub, hin1, hin2, length, k=5, hd=100) :
-    """
-    length of chars
-    """
-    ls = []
-    x = one_hot_encode()
-    while len(ls) < length :
-        hout1, hout2 = forward_pass(x, hin1, hin2, wub[:2])
-
-        # working softmax
-        d = np.dot(wub[2].data, hout2.data) + wub[3].data
-        np.exp(d, out=d)
-        sm = d / np.sum(d)
-        index = np.argmax(sm)
-
-        # tensor softmax not working, no idea why
-        # index = np.argmax(softmax(dense(wub[2], hout2) + wub[3]).data)
-
-        ls.append(keys[index])
-
-        x = one_hot_encode(ls[-1], 0)
-        hin1 = hout1
-        hin2 = hout2
-
-    return ls
-
-
-def sgd(param, rate) :
-    """
-    Performs a gradient-descent update on the parameter.
-
-    Parameters
-    ----------
-    param : mygrad.Tensor
-        The parameter to be updated.
-
-    rate : float
-        The step size used in the update"""
-    param.data -= rate * param.grad
-    return None
-
-
-def forward_pass(x, hin1, hin2, wub) :
-    """
-    Computes the forward pass of a step in the RNN
-
-    Parameters
-    -----------
-    x :
-
-    hin1 :
-
-    hin2 :
-
-    wub :
-    """
-    wub1, wub2 = wub
-    wz1, uz1, bz1, wr1, ur1, br1, wh1, uh1, bh1 = wub1
-    wz2, uz2, bz2, wr2, ur2, br2, wh2, uh2, bh2 = wub2
-
-    # first layer
-    zt1 = sigmoid(dense(uz1, x) + dense(wz1, hin1) + bz1)
-    rt1 = sigmoid(dense(ur1, x) + dense(wr1, hin1) + br1)
-    ht1 = tanh(dense(uh1, x) + dense(wh1, (hin1 * rt1)) + bh1)
-    hout1 = (1 - zt1) * hin1 + zt1 * ht1
-
-    # second layer
-    zt2 = sigmoid(dense(uz2, hout1) + dense(wz2, hin2) + bz2)
-    rt2 = sigmoid(dense(ur2, hout1) + dense(wr2, hin2) + br2)
-    ht2 = tanh(dense(uh2, hout1) + dense(wh2, (hin2 * rt2)) + bh2)
-
-    hout2 = (1 - zt2) * hin1 + zt2 * ht2
-
-    return hout1, hout2
-
-
-def cross_entropy(p_pred, p_true):
-    """ Computes the mean cross-entropy.
-
-        Parameters
-        ----------
-        p_pred : mygrad.Tensor, shape:(N, K)
-            N predicted distributions, each over K classes.
-
-        p_true : mygrad.Tensor, shape:(N, K)
-            N 'true' distributions, each over K classes
-
-        Returns
-        -------
-        mygrad.Tensor, shape=()
-            The mean cross entropy (scalar)."""
-    # return (-(1 - p_true) * log(1 - p_pred) - p_true * log(p_pred)).sum()
-    N = p_pred.shape[0]
-    p_logq = (p_true) * log(p_pred)
-    return (-1 / N) * p_logq.sum()
-
-
-def he_normal(shape):
-    """
-    Given the desired shape of your array, draws random
-        values from a scaled-Gaussian distribution.
-
-    Returns
-    --------
-    numpy.ndarray
-    """
-    N = shape[0]
-    scale = 1 / np.sqrt(2 * N)
-    return np.random.randn(*shape) * scale
-
-
-def one_hot_encode(data=None, i=None) :
-    """
-    optional params. both must be filled if one is. if neither, one hot encode a space
-    """
-    if (data is not None and i is None) or (data is None and i is not None) :
-        raise Exception("Either both data and i must be passed values, or neither are passed values")
-    elif data is not None and i is not None :
-        x = np.zeros((len(keys), 1))
-        x[keys.index(data[i])] = 1
-    else :
-        x = np.zeros((len(keys), 1))
-        x[-1] = 1
-    return x
-
-
-def import_midi(filename, sr) :
+def import_midi(filename) :
     """
     Converts midi file to ndarray of shape (num_ticks, num_notes)
         0 for note off; 1 for note on
+
+    Parameters
+    -----------
+    filename: r"PATH"
+        filepath of midi file
+
+    Returns
+    --------
+    str_song: str
+        string representation of song
     """
     midi_song = mido.MidiFile(filename)
 
+    # skips midi file if doesn't meet requirements
     if midi_song.type != 1 :
-        raise Exception("Midi file type not supported")
+        return
+        # raise Exception("Midi file type not supported")
     if midi_song.ticks_per_beat != 96 :
-        raise Exception("Number of ticks per beat must be 96")
+        return
+        # raise Exception("Number of ticks per beat must be 96")
 
     # assume standard 120 bpm
     # 128 is number of notes in midi, hence 127
@@ -273,8 +64,10 @@ def import_midi(filename, sr) :
     current_notes = []
     for i, track in enumerate(midi_song.tracks[1:]) :
         for msg in track:
+            # skips song if change in tempo; potentially confusing to RNN
             if msg.type == 'set_tempo' and msg.tempo != 500000 :
-                raise Exception("Tempo must be 120 BPM (500000 MSPB)")
+                return
+                #raise Exception("Tempo must be 120 BPM (500000 MSPB)")
 
             if not msg.is_meta :
                 if msg.type == 'note_on' and msg.velocity != 0 :
@@ -296,7 +89,9 @@ def import_midi(filename, sr) :
 
     ls_song = []
 
-    for tick in array_song[::sr,21:109] :
+    # converts ndarray representation to string representation for RNN
+    # slices out all non-piano notes of midi file
+    for tick in array_song[:, 20:108] :
         indices = np.where(tick == 1)
         indices = indices[0]
 
@@ -306,45 +101,89 @@ def import_midi(filename, sr) :
         ls_song.append("".join(temp_ls))
 
     str_song = " ".join(ls_song)
+    rnn_data.append(str_song)
 
-    return array_song, ls_song, str_song
+    # returns string representation of song
+    return str_song
 
-'''
-def str_to_arr(str_song) :
+
+def str_to_midi(str_song, dirname=None) :
     """
-    data = str
+    Converts string of note representations to midi file
+
+    Parameters
+    -----------
+    str_song: str
+        full string representation of midi song
+    dirname: r"PATH"
+        path to directory for midi file to be saved to
+
+    Returns
+    --------
+        mid: MidiFile
+            Mido MidiFile object
     """
-    arr_song = np.zeros((len(str_song.split()), len(keys) - 1))
+    arr_song = np.zeros((len(str_song.split(' ')), len(keys) - 1), dtype=int)
+    current_notes = []
     tick = 0
-    for char in data :
-        if char == keys[-1]
-            tick += 1
-        else :
-            arr_song[tick, keys.index(char)] = 1
 
-    return arr_song
+    # converts string representation to ndarry representation
+    for i, char in enumerate(str_song) :
+        if tick + 1 >= len(str_song):
+            break
+        else:
+            if char == ' ':
+                if len(current_notes) != 0:
+                    indices = np.array([keys.index(x) for x in current_notes])
+                    arr_song[tick, indices] = 1
+                    current_notes.clear()
+                tick += 1
+            elif char not in current_notes:
+                current_notes.append(char)
 
-
-def arr_to_midi(arr_song) :
-    """
-    arr_song = ndarray
-    """
+    # creates midi file
     mid = MidiFile()
     track = MidiTrack()
     mid.tracks.append(track)
+    mid.ticks_per_beat = 96
 
     current_notes = np.where(arr_song[0] == 1)[0].tolist()
-    # track.append(Message('note_on', note=64, velocity=64, time=32))
-    for tick in range(1, len(arr_song)) :
-        if arr_song[tick] != arr_song[tick - 1] :
+    prev_tick = 0
 
-        else :
-            continue
-'''
+    # converts ndarray representation to midi file
+    for i, msg in enumerate(current_notes):
+        track.append(Message('note_on', note=(21 + current_notes.index(msg)), velocity=64, time=0 + i))
+
+    for tick in range(1, len(arr_song)) :
+        ticks = np.sum((arr_song[tick], arr_song[tick - 1]), axis=0)
+        # need to check for if note is off or is on
+        if not np.array_equal(arr_song[tick], arr_song[tick - 1]) & len(ticks[ticks > 0]) != 0:
+            diff = np.where(arr_song[tick] != arr_song[tick - 1])[0]
+            for index in diff:
+                # if note was added
+                if arr_song[tick, index] == 1:
+                    track.append(Message('note_on', note=(21 + index), velocity=64, time=0))
+                # if note ended
+                else:
+                    track.append(Message('note_on', note=(21 + index), velocity=0, time=tick - prev_tick))
+            prev_tick = tick
+
+    current_notes = np.where(arr_song[len(arr_song) - 1] == 1)[0].tolist()
+    for msg in current_notes:
+        track.append(Message('note_on', note=(21 + current_notes.index(msg)), velocity=0, time=prev_tick))
+
+    if dirname is not None :
+        if os.path.exists(dirname):
+            mid.save(os.path.join(dirname, 'generated_song.mid'))
+    else :
+        mid.save('generated_song.mid')
+
+    return mid
+
 
 def save() :
     '''
     Saves face_data to a .pickle file
     '''
     with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "music_gen.pickle"), 'wb') as f:
-        pickle.dump(rnn_data, f, pickle.HIGHEST_PROTOCOL)
+        pickle.dump("   ".join(rnn_data), f, pickle.HIGHEST_PROTOCOL)
